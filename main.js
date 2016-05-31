0ea5757aaf359416d5a40fd3f30c121ad0a5fa92
kefala <martin.g.lobo@gmail.com>
2016-05-31 01:46:39 -0300

    Tp ready

--- main.js
+++ main.js
@@ -1,61 +1,52 @@
 function rle(stringToCompress) {
     if (stringToCompress[(stringToCompress.length - 1)] == 1) {
-        console.log("------------- Cadena: " + stringToCompress + "-------------");
-        rle2(stringToCompress);
-        rle3(stringToCompress);
-        rle4(stringToCompress);
+        alg(stringToCompress, 3, "rle-2");
+        alg(stringToCompress, 7, "rle-3");
+        alg(stringToCompress, 15, "rle-4");
+        alg(stringToCompress, 31, "rle-5");
+        document.querySelectorAll(".bytes-before").forEach(function(element) {
+            element.innerHTML = stringToCompress.length;
+        }, this);
+        
     }
 }
-function rle2(stringToCompress) {
-    alg(stringToCompress, 3, "rle-2");
-}
-function rle3(stringToCompress) {
-    alg(stringToCompress, 7, "rle-3");
-}
-function rle4(stringToCompress) {
-    alg(stringToCompress, 15, "rle-4");
-}
-
 
 function alg(stringToCompress, cota, rleName) {
     var groups = [];
-    var contBytes = 0;
+    var final = "";
     for (var i = 0; i < stringToCompress.length; i++) {
         var group = "";
-
-        while (stringToCompress[i] && stringToCompress[i] != "1" && group.length <= (cota-1)) {
+        while (stringToCompress[i] && stringToCompress[i] != "1" && group.length <= (cota - 1)) {
             group += stringToCompress[i++];
         }
-        
         if (group.length == cota || i == stringToCompress.length) {
             groups.push(group);
-            contBytes += group.length;
             i--;
         } else {
             group += "1";
             groups.push(group);
-            contBytes += group.length;
         }
     }
-    console.log(rleName);
-    console.log(groups);
+    groups.forEach(function (group) {
+        var cont = 0;
+        for (var i = 0; i < group.length; i++) {
+            if (group[i] == "0") cont++;
+        }
+        binaryCeros = parseFloat(cont).toString(2);
+        while (binaryCeros.length < parseInt(rleName[(rleName.length - 1)])) binaryCeros = "0" + binaryCeros;
+        final += binaryCeros;
+    }, this);
+    document.querySelector("." + rleName + " .final").innerHTML = final;
+    document.querySelector("." + rleName + " .bytes-after").innerHTML = final.length;
+    document.querySelector("." + rleName + " .tasa").innerHTML = (stringToCompress.length / final.length).toString().substring(0,4);
 }
-
-
 function init() {
     var input = document.querySelector("input");
     input.addEventListener('keyup', function (e) {
-        if (e.key == "0" || e.key == "1") {
-            rle(input.value);
-        }
+        if (e.key == "0" || e.key == "1") rle(input.value);
     }, false);
 }
-
-
 init();
-
-
-
 /*
 1   01
 3   11
